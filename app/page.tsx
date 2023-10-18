f01498cdf83a97b62f40da50d6cb528adf1ef849
usman akbar <vee7475@gmail.com>
2023-10-18 16:20:51 +0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Skills from "@/components/Skills";
+import Portfolio from "@/components/Portfolio";
+import Pricing from "@/components/Testimonial";
+import Testimonial from "@/components/Testimonial";
+
+export default function Home() {
+  return (
+    <main>
+      <>
+      <Hero />
+      <About />
+      <Skills />
+      <Portfolio />
+      <Pricing />
+      </>
+    </main>
+  )
+}
